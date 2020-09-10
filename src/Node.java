921b0863b07d16ea335e351e4ea3a0b3ba31364b
ExtraE113 <ezra@mamans.com>
2020-09-10 15:27:03 -0700

    generic versions

---
+++ src/Node.java
@@ -0,0 +1,38 @@
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+    private int visited;
+
+    public int getVisited() {
+        return visited;
+    }
+
+    public void setVisited(int visited) {
+        this.visited = visited;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public void insertAfter(Node<T> toInsert){
+        toInsert.next = next;
+        next = toInsert;
+    }
+
+    public Node(T data) {
+        this.data = data;
+    }
+}
